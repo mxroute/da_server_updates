819564b44ab6a956d4eefc7aa4f6161f2760499e
mxroute <37432698+mxroute@users.noreply.github.com>
2021-03-24 19:03:28 -0500

    Update update_exim.sh

--- exim/update_exim.sh
+++ exim/update_exim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Rebuild exim
+
 sh /usr/local/directadmin/custombuild/build update
 sh /usr/local/directadmin/custombuild/build exim
+
+# Refresh custom files with new copies
 
 rm -f /etc/exim.variables.conf.custom
 rm -f /etc/exim.easy_spam_fighter/variables.conf.custom
@@ -10,9 +14,12 @@ wget https://config.mxroute.com/update/exim/exim.variables.conf.custom -P /etc
 wget https://config.mxroute.com/update/exim/easy/variables.conf.custom -P /etc/exim.easy_spam_fighter
 wget https://config.mxroute.com/update/exim/exim.strings.conf.custom -P /etc
 
+# Rebuild exim config
+
 sh /usr/local/directadmin/custombuild/build exim_conf
 
-# Fix exim ACL
+# Fix problematic exim ACL
+
 if grep -q "#deny message = HELO_IS_LOCAL_DOMAIN" /etc/exim.conf
 then
         echo "Exim ACL already commented out."
@@ -21,5 +28,7 @@ else
         sed -i 's/condition = ${if match_domain{$sender_helo_name/#condition = ${if match_domain{$sender_helo_name/g' /etc/exim.conf
         sed -i 's/hosts = ! +relay_hosts/#hosts = ! +relay_hosts/g' /etc/exim.conf
 fi
+
+# If we don't kill exim before restarting it we cause downtime, if we do we face the tiniest of risks. Least risky play is kill -9 + restart.
 
 killall -9 exim && systemctl restart exim
