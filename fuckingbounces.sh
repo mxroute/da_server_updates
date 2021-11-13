75ab460f627889fb9c50e7dfebdf06f933d0f7a2
mxroute <37432698+mxroute@users.noreply.github.com>
2021-11-12 19:45:19 -0600

    Create fuckingbounces.sh

---
+++ fuckingbounces.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We don't forward emails that consistently get rejected, and that leads to bounces.
+# We're not ready to pick and choose who to send bounces to at the source server,
+# so the bounces stack up even though we're not delivering bounces to Google and Facebook.
+# This is a bandaid.
+
+chmod +x /root/fuckingbounces.sh
+
+if grep -q "fuckingbounces" /var/spool/cron/crontabs/root
+then
+  echo "Cron already installed."
+else
+  echo "0 * * * * /bin/bash /root/fuckingbounces.sh >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+fi
+
+for i in $(exim -bp | grep noreply-dmarc-support -B 1 | awk '{print $3}'); do exim -Mrm $i; done
+for i in $(exim -bp | grep facebookmail.com -B 1 | awk '{print $3}'); do exim -Mrm $i; done
