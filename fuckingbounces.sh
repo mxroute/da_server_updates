0a00f87e3e3c9d91d502503fa5a10671a8a27643
mxroute <37432698+mxroute@users.noreply.github.com>
2021-11-12 19:46:53 -0600

    Update fuckingbounces.sh

--- fuckingbounces.sh
+++ fuckingbounces.sh
@@ -5,13 +5,12 @@
 # so the bounces stack up even though we're not delivering bounces to Google and Facebook.
 # This is a bandaid.
 
-chmod +x /root/fuckingbounces.sh
-
 if grep -q "fuckingbounces" /var/spool/cron/crontabs/root
 then
   echo "Cron already installed."
 else
   echo "0 * * * * /bin/bash /root/fuckingbounces.sh >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
+  chmod +x /root/fuckingbounces.sh
 fi
 
 for i in $(exim -bp | grep noreply-dmarc-support -B 1 | awk '{print $3}'); do exim -Mrm $i; done
