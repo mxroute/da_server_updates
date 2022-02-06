fa0ff5e9f2b62cad4aff843967fa14bf168ac65f
mxroute <37432698+mxroute@users.noreply.github.com>
2022-02-06 16:31:28 -0600

    Update update_php73.sh

--- services/update_php73.sh
+++ services/update_php73.sh
@@ -2,4 +2,4 @@
 
 /usr/local/directadmin/custombuild/build update
 /usr/local/directadmin/custombuild/build set php1_release 7.3
-/usr/local/directadmin/plugins/custombuild/admin/build php
+/usr/local/directadmin/custombuild/build php
