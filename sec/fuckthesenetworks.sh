63f9c9eb8106e252df0c93b194125a4d0d691451
mxroute <37432698+mxroute@users.noreply.github.com>
2022-03-11 20:43:55 -0600

    Create fuckthesenetworks.sh

---
+++ sec/fuckthesenetworks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Sincerely, fuck these networks and the people who run them
+# These networks are not only home to massive amounts of spammers, but also to overall attackers
+# Blocking these networks will dramatically reduce brute force events on our servers,
+# and also somewhat mitigate against users who have had their password made public through some compromise across the internet
+# by reducing the malicious networks that can be used to test passwords from publicized databases
+# against customer email accounts
+
+ASNS="136647 202306 205090 211252 213035 28098 29182 399471 40065 40676 43830 49392 49870 62212"
+
+for a in $ASNS
+  do
+    for b in $(whois -h whois.radb.net -- "-i origin AS$a" | grep 'route:' | awk '{print $2}')
+      do ip route add blackhole $i
+    done
+done
