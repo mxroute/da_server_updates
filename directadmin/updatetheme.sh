a094fb2307ecd46904f7a4b0a1adc54a3016b02b
mxroute <37432698+mxroute@users.noreply.github.com>
2022-03-13 16:07:30 -0500

    Create updatetheme.sh

---
+++ directadmin/updatetheme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Deploy standardized DA layout for evolution theme
+
+mv /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/config.json /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/config.json$(date +%s)
+wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/directadmin/config.json -P /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution
+chown diradmin. /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/config.json
+systemctl restart directadmin
