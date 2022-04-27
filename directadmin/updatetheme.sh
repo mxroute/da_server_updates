7ca7d44416fcc913e34f296cc6e9d89eb7f13355
mxroute <37432698+mxroute@users.noreply.github.com>
2022-04-27 16:43:34 -0500

    Update updatetheme.sh

--- directadmin/updatetheme.sh
+++ directadmin/updatetheme.sh
@@ -3,5 +3,6 @@
 
 mv /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/config.json /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/config.json$(date +%s)
 wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/directadmin/config.json -P /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution
-chown diradmin. /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/config.json
+wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/directadmin/logo2.png -P /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution
+chown diradmin. /usr/local/directadmin/data/users/mxroute/skin_customizations/evolution/*
 systemctl restart directadmin
