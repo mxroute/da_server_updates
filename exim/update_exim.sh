f6251c89e3325abb14f375a3ab129e1b672286df
mxroute <37432698+mxroute@users.noreply.github.com>
2022-06-07 22:34:46 -0500

    Update update_exim.sh

--- exim/update_exim.sh
+++ exim/update_exim.sh
@@ -10,9 +10,9 @@ sh /usr/local/directadmin/custombuild/build exim
 rm -f /etc/exim.variables.conf.custom
 rm -f /etc/exim.easy_spam_fighter/variables.conf.custom
 rm -f /etc/exim.strings.conf.custom
-wget https://config.mxroute.com/update/exim/exim.variables.conf.custom -P /etc
-wget https://config.mxroute.com/update/exim/easy/variables.conf.custom -P /etc/exim.easy_spam_fighter
-wget https://config.mxroute.com/update/exim/exim.strings.conf.custom -P /etc
+cp /root/da_server_updates/exim/exim.variables.conf.custom /etc
+cp /root/da_server_updates/exim/easy/variables.conf.custom /etc/exim.easy_spam_fighter
+cp /root/da_server_updates/exim/exim.strings.conf.custom /etc
 
 # Rebuild exim config
 
@@ -21,18 +21,18 @@ sh /usr/local/directadmin/custombuild/build exim_conf
 # Add transport include
 
 rm -f /etc/exim.transports.pre.conf
-wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/exim/exim.transports.pre.conf -P /etc
+cp /root/da_server_updates/exim/exim.transports.pre.conf -P /etc
 
 # Add router include
 
 rm -f /etc/exim.routers.pre.conf
-wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/exim/exim.routers.pre.conf -P /etc
+cp /root/da_server_updates/exim/exim.routers.pre.conf -P /etc
 
 # Deploy custom exim.conf
 
 rm -f /etc/exim.conf.bak
 mv /etc/exim.conf /etc/exim.conf.bak
-wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/exim/exim.conf -P /etc
+cp /root/da_server_updates/exim/exim.conf -P /etc
 
 # If we don't kill exim before restarting it we cause downtime, if we do we face the tiniest of risks. Least risky play is kill -9 + restart.
 
