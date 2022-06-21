d59139ee85369b33c4dd8e11f3bb5353c29a9ae0
mxroute <37432698+mxroute@users.noreply.github.com>
2022-06-21 16:20:03 -0500

    Create installcloudsend.sh

---
+++ services/installcloudsend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo curl -o '/usr/local/bin/cloudsend' 'https://gist.githubusercontent.com/tavinus/93bdbc051728748787dc22a58dfe58d8/raw/cloudsend.sh' && sudo chmod +x /usr/local/bin/cloudsend
