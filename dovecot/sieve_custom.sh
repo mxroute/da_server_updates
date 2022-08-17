1cbdc0dd8998609f5a8af1d8aeab910150697232
mxroute <37432698+mxroute@users.noreply.github.com>
2022-08-17 14:30:24 -0500

    Create sieve_custom.sh

---
+++ dovecot/sieve_custom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f /etc/dovecot/conf.d/90-sieve.conf
+cp /root/da_server_updates/dovecot/90-sieve.conf /etc/dovecot/conf.d
+killall -9 dovecot
+systemctl restart dovecot
+systemctl status dovecot | grep "Active:"
