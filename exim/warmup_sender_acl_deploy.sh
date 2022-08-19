e562794b2d4194fa1d6f8c386fc2023140febc49
mxroute <37432698+mxroute@users.noreply.github.com>
2022-08-19 15:24:10 -0500

    Update warmup_sender_acl_deploy.sh

--- exim/warmup_sender_acl_deploy.sh
+++ exim/warmup_sender_acl_deploy.sh
@@ -7,3 +7,7 @@ cat >> /etc/exim.acl_check_recipient.pre.conf <<EOL
 deny    senders     = nwildlsearch*@;/etc/warmup_senders
         message     = Logged for review as likely involved with spam warmup service
 EOL
+
+# Restarting exim without killing it actually causes more downtime in our environment
+# So kill it, restart it, report that it's up
+killall -9 exim && systemctl restart exim && systemctl status exim | grep "Active:"
