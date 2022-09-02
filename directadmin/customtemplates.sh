89030b31175599538088586cb9fd16fce4ec6c82
mxroute <37432698+mxroute@users.noreply.github.com>
2022-09-02 17:48:29 -0500

    Create customtemplates.sh

---
+++ directadmin/customtemplates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in $(ls /root/da_server_updates/directadmin/templates/custom); do rm -f /usr/local/directadmin/data/templates/custom/$i && cp /root/da_server_updates/directadmin/templates/custom/$i /usr/local/directadmin/data/templates/custom; done
+
+chown -R diradmin. /usr/local/directadmin/data/templates/custom
