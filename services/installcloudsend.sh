0ac939a4dde57d37eddd79931eb1637b1bb462a5
mxroute <37432698+mxroute@users.noreply.github.com>
2022-10-09 15:25:28 -0500

    Update installcloudsend.sh

--- services/installcloudsend.sh
+++ services/installcloudsend.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# This is installed on servers to help quickly send account backups to NextCloud.
+# Primarily used when accounts are terminated for policy violations.
+# We'll create a folder in NextCloud, set it to shared and to accept uploads, and then set a password on it.
 
 sudo curl -o '/usr/local/bin/cloudsend' 'https://gist.githubusercontent.com/tavinus/93bdbc051728748787dc22a58dfe58d8/raw/cloudsend.sh' && sudo chmod +x /usr/local/bin/cloudsend
