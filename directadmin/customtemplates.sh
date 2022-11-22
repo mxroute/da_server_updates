d3fc35196e8826118f2aa6363bb5eb3208456708
mxroute <37432698+mxroute@users.noreply.github.com>
2022-11-22 16:21:30 -0600

    Update customtemplates.sh

--- directadmin/customtemplates.sh
+++ directadmin/customtemplates.sh
@@ -2,5 +2,6 @@
 
 for i in $(ls /root/da_server_updates/directadmin/templates/custom); do rm -f /usr/local/directadmin/data/templates/custom/$i && cp /root/da_server_updates/directadmin/templates/custom/$i /usr/local/directadmin/data/templates/custom; done
 
+mkdir -P /usr/local/directadmin/data/templates/custom
 chown -R diradmin. /usr/local/directadmin/data/templates/custom
 chmod 0644 /usr/local/directadmin/data/templates/custom/*
