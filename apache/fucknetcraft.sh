636d53af23757674f6cd03f7114317f45ebadcc0
mxroute <37432698+mxroute@users.noreply.github.com>
2022-12-05 13:28:22 -0600

    Delete fucknetcraft.sh
    We're cool now.

--- apache/fucknetcraft.sh
+++
@@ -1,5 +0,0 @@
-# Removing the redirects to webmail because Netcraft is so fucking incompetent that they want to try to ruin our business over having redirects.
-# This file is a big fuck you to them. Eat a dick, Netcraft.
-# Having a rediect is not equal to every redirect being a phishing page, but thanks for the millionth fucking abuse complaint.
-
-cp /root/da_server_updates/apache/index.html /var/www/html
