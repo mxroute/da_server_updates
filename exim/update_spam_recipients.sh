01a7cce44977aa81418685b9c9d3371ff10cc6f7
mxroute <37432698+mxroute@users.noreply.github.com>
2023-02-04 18:39:24 -0600

    Update update_spam_recipients.sh

--- exim/update_spam_recipients.sh
+++ exim/update_spam_recipients.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(cat /root/da_server_updates/exim); do echo "$i" >> /etc/spam_recipients; done
+for i in $(cat /root/da_server_updates/exim/spam_recipients); do echo "$i" >> /etc/spam_recipients; done
 sort /etc/spam_recipients | uniq >> /etc/spam_recipients2
 rm -f /etc/spam_recipients
 mv /etc/spam_recipients2 /etc/spam_recipients
