bd24431afa6ae0f1e0da8ec9409ee399b291cbfa
mxroute <37432698+mxroute@users.noreply.github.com>
2023-04-03 18:37:35 -0500

    Update sieve_custom.sh

--- dovecot/sieve_custom.sh
+++ dovecot/sieve_custom.sh
@@ -3,5 +3,5 @@
 rm -f /etc/dovecot/conf.d/90-sieve.conf
 cp /root/da_server_updates/dovecot/90-sieve.conf /etc/dovecot/conf.d
 killall -9 dovecot
-systemctl restart dovecot
+systemctl reload dovecot
 systemctl status dovecot | grep "Active:"
