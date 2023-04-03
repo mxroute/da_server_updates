6c2364f6320c35cb211fee72aecca3f6830f04a2
mxroute <37432698+mxroute@users.noreply.github.com>
2023-04-03 18:53:25 -0500

    Update sieve_custom.sh

--- dovecot/sieve_custom.sh
+++ dovecot/sieve_custom.sh
@@ -2,6 +2,8 @@
 
 rm -f /etc/dovecot/conf.d/90-sieve.conf
 cp /root/da_server_updates/dovecot/90-sieve.conf /etc/dovecot/conf.d
-killall -9 dovecot
 systemctl reload dovecot
-systemctl status dovecot | grep "Active:"
+
+# I know it doesn't belong here but I'm doing it anyway
+rm -f /etc/mail/spamassassin/local.cf
+wget https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/local.cf -O /etc/mail/spamassassin/local.cf
