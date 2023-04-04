c9965e0799bfbe4d3ab673ccd8244ed69f913bfa
mxroute <37432698+mxroute@users.noreply.github.com>
2023-04-03 19:45:04 -0500

    Update check_mx.sh

--- sec/check_mx.sh
+++ sec/check_mx.sh
@@ -6,14 +6,16 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Create a temporary file for domains with MX records
+# Create a temporary file for domains without MX records
 temp_file=$(mktemp)
 
 # Iterate through each domain in the input file
 while read -r domain; do
-  # Use nslookup to check if the domain has MX record
-  if nslookup -q=mx "$domain" 8.8.8.8 >/dev/null; then
+  # Use dig to check if the domain has MX record
+  if dig +nocmd +noall +answer -t MX "$domain" "@8.8.8.8" | grep -q MX; then
+    # Remove domain from input file
     echo "$domain has MX record. Removing from input file."
+  else
     # Add domain to temporary file
     echo "$domain" >> "$temp_file"
   fi
