a4ca0ac0867dbb1e91edff2c7ddd73f7697d3a66
mxroute <37432698+mxroute@users.noreply.github.com>
2023-04-24 17:37:23 -0500

    Create move_mxrbl_to_sa.sh

---
+++ exim/move_mxrbl_to_sa.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /etc/exim.strings.conf.custom
+cp /root/da_server_updates/exim/exim.strings.conf.custom /etc
+killall -9 exim
+systemctl restart exim
+systemctl status exim | grep Active:
