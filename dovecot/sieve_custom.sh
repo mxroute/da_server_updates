bf3d8304efa19fd8a15b13d450577f23c4cbf9ec
mxroute <37432698+mxroute@users.noreply.github.com>
2023-04-27 20:18:19 -0500

    Update sieve_custom.sh

--- dovecot/sieve_custom.sh
+++ dovecot/sieve_custom.sh
@@ -3,7 +3,3 @@
 rm -f /etc/dovecot/conf.d/90-sieve.conf
 cp /root/da_server_updates/dovecot/90-sieve.conf /etc/dovecot/conf.d
 systemctl reload dovecot
-
-# I know it doesn't belong here but I'm doing it anyway
-rm -f /etc/mail/spamassassin/local.cf
-wget https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/local.cf -O /etc/mail/spamassassin/local.cf
