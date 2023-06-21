558d33d82acea9112bbce61b376bc3a82df8b16c
mxroute <37432698+mxroute@users.noreply.github.com>
2023-06-21 01:08:00 -0500

    Update move_mxrbl_to_sa.sh

--- exim/move_mxrbl_to_sa.sh
+++ exim/move_mxrbl_to_sa.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Adapting to kill off RBLs outside of SA entirely
+unlink /etc/virtual/use_rbl_domains
+touch /etc/virtual/use_rbl_domains
+chown mail. /etc/virtual/use_rbl_domains
+chmod 0644 /etc/virtual/use_rbl_domains
+
+# The original version of this script, still helpful for processing the above
 rm -f /etc/exim.strings.conf.custom
 cp /root/da_server_updates/exim/exim.strings.conf.custom /etc
 killall -9 exim
