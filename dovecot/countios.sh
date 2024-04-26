175b9ea95ae5b860cdd731b8ee7eb518c7781249
mxroute <37432698+mxroute@users.noreply.github.com>
2024-04-25 19:57:01 -0500

    Update countios.sh

--- dovecot/countios.sh
+++ dovecot/countios.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep "Notification sent successfully" maillog | awk -F'imap\\(' '{print $2}' | awk -F')' '{print $1}' | sort | uniq | wc -l
+grep "Notification sent successfully" /var/log/maillog | awk -F'imap\\(' '{print $2}' | awk -F')' '{print $1}' | sort | uniq | wc -l
