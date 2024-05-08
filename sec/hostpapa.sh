147c82bf87042f8d8f34074a05f1f00ef1640feb
mxroute <37432698+mxroute@users.noreply.github.com>
2024-05-08 10:45:21 -0500

    Update hostpapa.sh

--- sec/hostpapa.sh
+++ sec/hostpapa.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script is step 1 in providing HostPapa with actionable abuse complaints about spam leaving their network
 
-grep -wF -f /root/da_server_updates/sec/hostpapa_ips /var/log/exim/mainlog* >> /root/da_server_updates/sec/hostpapa_logs
+#grep -wF -f /root/da_server_updates/sec/hostpapa_ips /var/log/exim/mainlog* >> /root/da_server_updates/sec/hostpapa_logs
+for i in $(ps faux | grep hostpapa | grep -v grep | awk '{print $2}'; do kill -9 $i; done
