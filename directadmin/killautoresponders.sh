745007bea69783f548364a4fc7d13963b9ad877e
mxroute <37432698+mxroute@users.noreply.github.com>
2024-05-17 17:09:12 -0500

    Update killautoresponders.sh

--- directadmin/killautoresponders.sh
+++ directadmin/killautoresponders.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DOMAINLIST=$(cat /etc/virtual/domains)
+# Kill autoresponders created with DirectAdmin
 
+DOMAINLIST=$(cat /etc/virtual/domains)
 for i in $DOMAINLIST
   do
     rm -f /etc/virtual/$i/autoresponder.conf
@@ -10,3 +11,7 @@ for i in $DOMAINLIST
     chmod 0600 /etc/virtual/$i/autoresponder.conf
     rm -f /etc/virtual/$i/reply/*
   done
+
+# Kill the ability to create autoresponders with Roundcube
+
+sed -i "s/\$config\['managesieve_vacation'\] = 1/\$config\['managesieve_vacation'\] = 0/" /var/www/html/roundcube/plugins/managesieve/config.inc.php
