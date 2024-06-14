3cd72a8324fe426c25b1031f6e7afc7f1f05b26d
mxroute <37432698+mxroute@users.noreply.github.com>
2024-06-14 16:14:15 -0500

    Update gather_invalids.sh

--- exim/gather_invalids.sh
+++ exim/gather_invalids.sh
@@ -5,6 +5,6 @@ for i in $(grep -a "Recipient address rejected: Domain not found" /var/log/exim/
 		for a in $(grep -a $i /var/log/exim/mainlog | grep "Domain not found" | grep -v "cuoly.com" | grep -v "askjdmiller.com" | awk -F'==' '{print $2}' | awk '{print $1}' | awk -F'@' '{print $2}' | sort | uniq)
 			do
 				echo "$a" >> /var/log/exim/spam_recipient_staging
-				for i in $(exim -bp | grep -v "D " | grep $a -B 1 | awk '{print $3}'); do exim -Mrm $i; done
+				for i in $(exim -bp | grep -v "D " | grep $a -B 1 | awk '{print $3}'); do exim -Mg $i; done
 			done
 	done
