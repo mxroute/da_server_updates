76f29f63e57caa556b9ee50adfb63999a841dc69
mxroute <37432698+mxroute@users.noreply.github.com>
2024-06-14 16:32:10 -0500

    Update clearcertlogs.sh

--- exim/clearcertlogs.sh
+++ exim/clearcertlogs.sh
@@ -2,4 +2,8 @@
 # I don't need to renew SSL certificates for internal server to server communication, and I'm tired of customers asking me about it.
 # In a highly controlled environment, suppressing this error serves no purpose other than to reduce customer questions.
 
-sed -i '/certificate has expired cert/d' /var/log/exim/mainlog
+if [ -d "/var/cpanel" ]; then
+  sed -i '/certificate has expired cert/d' /var/log/exim_mainlog
+else
+  sed -i '/certificate has expired cert/d' /var/log/exim/mainlog
+fi
