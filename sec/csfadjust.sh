a767cf59c172c3f5308090707ebb90c4995285f7
mxroute <37432698+mxroute@users.noreply.github.com>
2024-06-14 15:25:00 -0500

    Create csfadjust.sh

---
+++ sec/csfadjust.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Step 1: Change LF_DISTSMTP to 0
+sed -i '/^LF_DISTSMTP =/c\LF_DISTSMTP = "0"' /etc/csf/conf
+
+# Step 2: Restart CSF
+csf -r
+
+# Step 3: Unban any IPs caught in this rule
+for i in $(grep "distributed smtpauth" csf.deny | awk '{print $1}'); do csf -dr $i; done
