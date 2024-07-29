9c7f29ae88b33bac87c7c8add93e399f07d12876
mxroute <37432698+mxroute@users.noreply.github.com>
2024-07-29 12:55:40 -0500

    Update update_acls.sh

--- exim/update_acls.sh
+++ exim/update_acls.sh
@@ -6,3 +6,4 @@ rm -f /etc/spam_recipients
 cp /root/da_server_updates/exim/spam_recipients /etc
 rm -f /etc/exim.acl_check_recipient.pre.conf
 cp /root/da_server_updates/exim/exim.acl_check_recipient.pre.conf /etc
+cp /root/da_server_updates/exim/exim.acl_check_message.pre.conf /etc
