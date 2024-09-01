7483532f7b56a24a7d84a48f0f4b8206fcd93435
mxroute <37432698+mxroute@users.noreply.github.com>
2024-08-31 23:34:43 -0500

    Update emailaudit.sh

--- directadmin/emailaudit.sh
+++ directadmin/emailaudit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find /var/log/directadmin -name "emailaudit.log" -delete
-grep "created by" /var/log/directadmin/system.log.1 | grep Email | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq -c | sort -n | tail -10 >> /var/log/directadmin/emailaudit.log
+grep "created by" /var/log/directadmin/system.log | grep Email | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq -c | sort -n | tail -10 >> /var/log/directadmin/emailaudit.log
