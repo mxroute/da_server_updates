965652f86b6b01b0e065033ecc9469e12dfd360b
mxroute <37432698+mxroute@users.noreply.github.com>
2024-10-30 14:13:52 -0500

    Update update_rules.sh

--- spamassassin/update_rules.sh
+++ spamassassin/update_rules.sh
@@ -2,3 +2,5 @@
 rm -f /etc/mail/spamassassin/local.cf
 wget https://raw.githubusercontent.com/mxroute/da_server_updates/master/spamassassin/local.cf -P /etc/mail/spamassassin
 systemctl restart spamd
+rm -f /etc/susranges
+wget https://raw.githubusercontent.com/mxroute/da_server_updates/refs/heads/master/exim/susranges -P /etc
