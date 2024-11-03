8426a45ac4f1623577b5f1ff2822514987dfcda0
mxroute <37432698+mxroute@users.noreply.github.com>
2024-11-03 17:28:26 -0600

    Update update_acls.sh

--- exim/update_acls.sh
+++ exim/update_acls.sh
@@ -6,3 +6,4 @@ rm -f /etc/exim.acl_check_recipient.pre.conf
 cp /root/da_server_updates/exim/exim.acl_check_recipient.pre.conf /etc
 rm -f /etc/exim.acl_check_message.pre.conf
 cp /root/da_server_updates/exim/exim.acl_check_message.pre.conf /etc
+killall -9 exim && systemctl restart exim
