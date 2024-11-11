18af5c204f776f81519eae6167d6a3afd41d6614
mxroute <37432698+mxroute@users.noreply.github.com>
2024-11-11 16:29:31 -0600

    Update add_bulk_acls.sh

--- exim/add_bulk_acls.sh
+++ exim/add_bulk_acls.sh
@@ -2,4 +2,6 @@
 
 echo "" >> /etc/exim.acl_check_recipient.pre.conf
 cat /root/da_server_updates/exim/exim.acl_check_recipient.pre.conf >> /etc/exim.acl_check_recipient.pre.conf
+rm -f /etc/bannedspoofing
+wget -O /etc/bannedspoofing https://raw.githubusercontent.com/mxroute/da_server_updates/refs/heads/master/exim/bannedspoofing
 killall -9 exim && systemctl restart exim
