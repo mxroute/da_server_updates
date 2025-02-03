19499c929067dfac1e84cffd97688296701ab065
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-02 21:59:03 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -4,4 +4,4 @@
 SERVER_IP=$(curl -s ifconfig.me)
 
 # Search Exim logs for both H= connections and authentication failures from our IP
-grep -E "H=\(${SERVER_IP}\)|authenticator failed for \(${SERVER_IP}\)" /var/log/exim/mainlog
+grep -a -E "H=\(${SERVER_IP}\)|authenticator failed for \(${SERVER_IP}\)" /var/log/exim/mainlog
