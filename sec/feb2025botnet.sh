4a94dcb8453a5c37a796452acdcbff93d5df57b9
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-02 22:10:37 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -4,8 +4,8 @@
 SERVER_IP=$(curl -4 -s ifconfig.me)
 
 # Search Exim logs, strictly matching dotted decimal IP format
-# Return list of IPs involved, use pastebin
-grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./[.]}\)" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sed 's/://' | sort | uniq | grep -v 127.0.0.1 | curl -F 'sprunge=<-' https://mxbin.io
+# Return list of IPs involved
+grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./[.]}\)" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sed 's/://' | sort | uniq | grep -v 127.0.0.1
 
 # Blackhole the IPs involved
 for i in $(grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./[.]}\)" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sed 's/://' | sort | uniq | grep -v 127.0.0.1); do ip route add blackhole $i; done
