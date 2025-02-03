769a25c7e394f9f0b45f2fa331b0f111041347c0
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-03 13:49:03 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -16,6 +16,9 @@ for i in $(grep "Unauthenticated mail not allowed from this range" /var/log/exim
 # Another list
 for i in $(grep "Relay not permitted" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq); do ip route add blackhole $i; done
 
+# Thank you, next
+for i in $(grep "Relay not permitted" /var/log/exim/mainlog | awk -F'F=' '{print $1}' | grep -v -F "H=("  | grep -v "X=" | awk -F'\\[' '{print $2}' | sed 's/]//' | sort | uniq); do ip route add blackhole $i; done
+
 # Restart exim, this can't wait for procs to close naturally
 killall -9 exim
 systemctl restart exim
