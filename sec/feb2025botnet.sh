58b442fbc5a0c257009cacc7a5ccd368e8aa29cb
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-03 13:43:12 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -9,3 +9,10 @@ grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./
 
 # Blackhole the IPs involved
 for i in $(grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./[.]}\)" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sed 's/://' | sort | uniq | grep -v 127.0.0.1); do ip route add blackhole $i; done
+
+# New list
+for i in $(grep "Unauthenticated mail not allowed from this range" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq); do ip route add blackhole $i; done
+
+# Restart exim, this can't wait for procs to close naturally
+killall -9 exim
+systemctl restart exim
