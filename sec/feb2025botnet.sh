948d59279bca87d5a3aaf30dc3b6e6e3bec3f520
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-02 22:06:23 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -4,5 +4,5 @@
 SERVER_IP=$(curl -4 -s ifconfig.me)
 
 # Search Exim logs, strictly matching dotted decimal IP format
-# Use [.] to ensure only literal dots are matched within the parentheses
-grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./[.]}\)" /var/log/exim/mainlog
+# Return list of IPs involved
+grep -a -E "H=\(${SERVER_IP//./[.]}\)|authenticator failed for \(${SERVER_IP//./[.]}\)" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sed 's/://' | sort | uniq | grep -v 127.0.0.1
