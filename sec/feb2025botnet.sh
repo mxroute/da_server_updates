f964a996e294b3a66459d0fd3b0db18ed0e7417d
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-14 01:09:35 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in $(grep "Relay not permitted" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "Relay not permitted" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
-for i in $(grep "Mail not accepted from default assigned hostnames" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "Mail not accepted from default assigned hostnames" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
-for i in $(grep "Unauthenticated mail not allowed from this range" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "Unauthenticated mail not allowed from this range" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
 for i in $(cat /etc/unblockme); do ip route del blackhole $i; done
 
