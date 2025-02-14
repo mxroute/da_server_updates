c59267b9998b40d686fb3860880eb2f3e68cd316
mxroute <37432698+mxroute@users.noreply.github.com>
2025-02-14 01:03:34 -0600

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -7,3 +7,6 @@ for i in $(grep "Mail not accepted from default assigned hostnames" /var/log/exi
 for i in $(grep "Unauthenticated mail not allowed from this range" /var/log/exim/mainlog | awk -F'\\) \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
 for i in $(cat /etc/unblockme); do ip route del blackhole $i; done
+
+killall -9 exim
+systemctl restart exim
