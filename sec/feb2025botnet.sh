549844a7e01ec0f1b20930c7af91c9bf4d91834f
mxroute <37432698+mxroute@users.noreply.github.com>
2025-03-14 02:00:03 -0500

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -8,6 +8,8 @@ for i in $(grep -a "Unauthenticated mail not allowed from this range" /var/log/e
 
 for i in $(grep -a "is not authorized to send mail from" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
+for i in $(grep -a "Too many failed recipients" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 10 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+
 for i in $(cat /etc/unblockme); do ip route del blackhole $i; done
 
 killall -9 exim
