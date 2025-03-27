94c1232d3b9446e10ea8ed245bdca36741408caf
mxroute <37432698+mxroute@users.noreply.github.com>
2025-03-27 08:27:41 -0500

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for i in $(grep -a "Relay not permitted" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 100 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "Relay not permitted" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
-for i in $(grep -a "Mail not accepted from default assigned hostnames" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 100 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "Mail not accepted from default assigned hostnames" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
-for i in $(grep -a "Unauthenticated mail not allowed from this range" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 100 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "Unauthenticated mail not allowed from this range" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
-for i in $(grep -a "is not authorized to send mail from" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 100 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
+for i in $(grep -a "is not authorized to send mail from" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq -c | sort -n | awk '$1 >= 30 && NF == 2' | awk '{print $2}'); do ip route add blackhole $i; done
 
 for i in $(grep -a "Too many failed recipients" /var/log/exim/mainlog | awk -F' \\[' '{print $2}' | awk '{print $1}' | sed 's/]//' | sort | uniq); do ip route add blackhole $i; done
 
@@ -14,7 +14,5 @@ for i in $(grep "Google Cloud has conditional access" /var/log/exim/mainlog | aw
 
 for i in $(cat /etc/unblockme); do ip route del blackhole $i; done
 
-for i in $(cat /etc/susranges_whitelist); do ip route del blackhole $i; done
-
-#killall -9 exim
-#systemctl restart exim
+killall -9 exim
+systemctl restart exim
