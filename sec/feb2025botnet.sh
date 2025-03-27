fb27257c08cd4d59c90cd13a3034945a1616666d
mxroute <37432698+mxroute@users.noreply.github.com>
2025-03-26 20:53:19 -0500

    Update feb2025botnet.sh

--- sec/feb2025botnet.sh
+++ sec/feb2025botnet.sh
@@ -14,5 +14,7 @@ for i in $(grep "Google Cloud has conditional access" /var/log/exim/mainlog | aw
 
 for i in $(cat /etc/unblockme); do ip route del blackhole $i; done
 
+for i in $(cat /etc/susranges_whitelist); do ip route del blackhole $i; done
+
 killall -9 exim
 systemctl restart exim
