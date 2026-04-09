f8b96ef902edd2372269fe51b5c84f5dbc47e360
mxroute <37432698+mxroute@users.noreply.github.com>
2026-04-09 18:11:24 -0500

    Update update_roundcube.sh

--- roundcube/update_roundcube.sh
+++ roundcube/update_roundcube.sh
@@ -1,74 +1,15 @@
 #!/bin/bash
+set -e
 
-# Set Variables
+WORK="/root/working/roundcube"
+VERSION="1.6.13"
+TARGET="/var/www/html/roundcubemail-1.6.11"
+TARBALL="roundcubemail-${VERSION}.tar.gz"
+URL="https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/${TARBALL}"
 
-RCMYSQLPASS=$(grep "password" /var/www/html/roundcube/config/my.cnf | sed 's/password=//')
-RCPLUGINS=(persistent_login advanced_search)
-
-# Make backup
-
-rm -rf /root/temp/backups/roundcube
-mkdir -p /root/temp/backups/roundcube
-cp -R /var/www/html/roundcube /root/temp/backups/roundcube
-
-# Run update
-
-/usr/local/directadmin/custombuild/build roundcube
-
-# Set SSL Redirect
-
-if grep -q "RewriteCond" /var/www/html/roundcube/.htaccess
-then
-        echo "Roundcube SSL redirect already in place."
-else
-        sed -i '1 s/^/RewriteRule \(\.\*\) https\:\/\/\%\{HTTP_HOST\}\%\{REQUEST\_URI\} \[R\=301\,L\]\n/' /var/www/html/roundcube/.htaccess
-        sed -i '1 s/^/RewriteCond \%\{HTTPS\} off\n/' /var/www/html/roundcube/.htaccess
-        sed -i '1 s/^/RewriteEngine On\n/' /var/www/html/roundcube/.htaccess
-fi
-
-# Install persistent_login
-
-if [ -d "/var/www/html/roundcube/plugins/persistent_login" ]
-then
-	echo "Persistent login already installed."
-else
-	yum install git -y
-	cd /var/www/html/roundcube/plugins
-	git clone https://github.com/texxasrulez/persistent_login
-	mv /var/www/html/roundcube/plugins/persistent_login/config.inc.php.dist /var/www/html/roundcube/plugins/persistent_login/config.inc.php
-	chown -R webapps. /var/www/html/roundcube/plugins
-	mysql -uda_roundcube -p"$RCMYSQLPASS" da_roundcube < /var/www/html/roundcube/plugins/persistent_login/SQL/mysql.initial.sql
-fi
-
-# Install advanced_search
-
-	cd /var/www/html/roundcube/plugins
-	git clone https://github.com/mxroute/advanced_search
-	mv /var/www/html/roundcube/plugins/advanced_search/config.inc.php.dist /var/www/html/roundcube/plugins/advanced_search/config.inc.php
-	chown -R webapps. /var/www/html/roundcube/plugins
-
-# Add plugins to config
-
-if grep -q ${RCPLUGINS[0]} /var/www/html/roundcube/config/config.inc.php
-then
-        echo "Plugins already installed."
-else
-        for i in ${RCPLUGINS[@]}
-        do
-		sed -i "s/managesieve',/managesieve',\n    '$i',/g" /var/www/html/roundcube/config/config.inc.php
-		echo "$i installed."
-        done
-fi
-
-# Fix managesieve plugin config
-rm -f /var/www/html/roundcube/plugins/managesieve/config.inc.php
-cp /root/da_server_updates/roundcube/managesieve/config.inc.php /var/www/html/roundcube/plugins/managesieve
-chown webapps. /var/www/html/roundcube/plugins/managesieve/config.inc.php
-
-# Rate limit login attempts
-cat >> /var/www/html/roundcube/config/config.inc.php <<'EOF'
-$config['login_rate_limit'] = 4;
-EOF
-
-unset RCMYSQLPASS
-unset RCPLUGINS
+rm -rf "$WORK"
+mkdir -p "$WORK"
+wget -q -O "${WORK}/${TARBALL}" "$URL"
+tar -zxf "${WORK}/${TARBALL}" -C "$WORK"
+echo "y" | "${WORK}/roundcubemail-${VERSION}/bin/installto.sh" "$TARGET"
+chown -R webapps: "$TARGET"
